95bd6eeceb31225a012295342d9108ff9d9b5238
igor <igorsife80@gmail.com>
2022-03-30 20:06:39 -0300

    adiciona cors e permite tela de login

--- src/api/app.js
+++ src/api/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const path = require('path');
-/* const cors = require('cors'); */
+ const cors = require('cors'); 
 
 const port = process.env.PORT || 3001;
 const app = express();
 const http = require('http').createServer(app);
 
-/* const io = require('socket.io')(http, {
+ const io = require('socket.io')(http, {
     cors: {
-      origin: 'http://localhost:3000', // url aceita pelo cors
+      origin: ['http://localhost:3000', 'https://in-tregasdelivery.herokuapp.com/'], // url aceita pelo cors
       methods: ['GET', 'POST'], // Métodos aceitos pela url
-    } }); */
-
+    } });
 const root = require('../controllers/root');
 const { errorHandler } = require('../middlewares');
 
-/* app.use(cors()); */
+ app.use(cors()); 
 
 app.use(express.json());
 
@@ -26,7 +25,7 @@ app.get('/images/:name', async (req, res, _next) => {
 
 app.use(root);
 
-/* require('../sockets/saleStatus')(io); */
+require('../sockets/saleStatus')(io); 
 
 app.use(errorHandler);
 
