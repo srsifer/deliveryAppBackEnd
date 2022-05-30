57aa496866ff7970663942cc162124c60230dbd3
igor s fernandes <igorsife80@gmail.com>
2022-05-30 18:01:58 -0300

    set PORT var

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,6 @@
 const http = require('./app');
 
-const port = process.env.API_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 http.listen(port);
 console.log(`Api rodando na porta ${port}`);
